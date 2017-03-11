20eeb8c49f7dad4921d3940947868970b6a60aeb
asksven <sven.knispel@gmail.com>
2017-03-11 11:03:58 +0100

    Removed display of details (event-bug)

--- src/app/leaders/leader-details/leader-details.component.ts
+++ src/app/leaders/leader-details/leader-details.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 import { Achievement } from '../achievement'
 import { Leader } from '../leader';
@@ -10,17 +10,10 @@ import { Leader } from '../leader';
 })
 export class LeaderDetailsComponent implements OnInit {
 
-  @Input() selectedLeader: Leader;
   achievements: Achievement[] = []
-  //achievement: Achievement = new Achievement("CCT", "Optimized Azure FW Process", "Instead of trying to keep up pace and generate deltas for changes within the 2800 Azure Subnets the process now deletes and re-creates all subnets for every change", 60);
+  achievement: Achievement = new Achievement("CCT", "Optimized Azure FW Process", "Instead of trying to keep up pace and generate deltas for changes within the 2800 Azure Subnets the process now deletes and re-creates all subnets for every change", 60);
   constructor() { }
     
- getNotification(leader)
-  {
-    console.log("LeaderDetailsComponent.getNotification received " + leader);
-    this.selectedLeader = leader;
-    
-  }
   ngOnInit() {
   }
 
